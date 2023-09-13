94f6f15b96c634b577eb258ecf068ab50533d929
Justin Cianci <justinc328@gmail.com>
2023-09-12 22:21:51 -0400

    react router

--- src/frontend/app/src/components/navbar/Navbar.tsx
+++ src/frontend/app/src/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import BsNavbar from 'react-bootstrap/Navbar';
+import styles from './Navbar.module.scss';
 
 
 const Navbar: React.FC = () => {
@@ -10,16 +12,24 @@ const Navbar: React.FC = () => {
     <BsNavbar sticky="top" bg="white" className="border-bottom" expand="lg">
       <Container fluid className="mx-md-3">
         <BsNavbar.Brand>
-          <strong>Finances</strong>
+          <Link className={styles["link"]} to="/"><strong>Finances</strong></Link>
         </BsNavbar.Brand>
 
         <BsNavbar.Toggle aria-controls="navbarDropdown" />
         <BsNavbar.Collapse id="navbarDropdown">
           <Nav>
-            <Nav.Link href="#accounts">Accounts</Nav.Link>
-            <Nav.Link href="#paychecks">Paychecks</Nav.Link>
-            <Nav.Link href="#transactions">Transactions</Nav.Link>
-            <Nav.Link href="#categories">Categories</Nav.Link>
+            <Nav.Link>
+              <Link className={styles["link"]} to="/accounts">Accounts</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className={styles["link"]} to="/paychecks">Paychecks</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className={styles["link"]} to="/transactions">Transactions</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link className={styles["link"]} to="/categories">Categories</Link>
+            </Nav.Link>
           </Nav>
         </BsNavbar.Collapse>
 
--- src/frontend/app/src/index.tsx
+++ src/frontend/app/src/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/accounts",
+    element: <App />,
+  },
+  {
+    path: "/paychecks",
+    element: <App />,
+  },
+  {
+    path: "/transactions",
+    element: <App />,
+  },
+  {
+    path: "/categories",
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
